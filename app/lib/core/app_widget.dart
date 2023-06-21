b921e955802981244dcf1792fe440b48bdeb2ece
LuiFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-06-21 14:19:24 -0300

    chore: :lipstick: configure button themes

--- app/lib/core/app_widget.dart
+++ app/lib/core/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../modules/splash/views/splash_page.dart';
+import 'shared/app_colors.dart';
+import 'shared/app_text_styles.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -10,7 +12,38 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
+        fontFamily: 'Jost',
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            backgroundColor: MaterialStatePropertyAll<Color>(
+              AppColors.darkBlue,
+            ),
+            textStyle: MaterialStatePropertyAll<TextStyle>(
+              AppTextStyles.textBold,
+            ),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStatePropertyAll<Color>(AppColors.primary),
+            textStyle: MaterialStatePropertyAll<TextStyle>(
+              AppTextStyles.body500,
+            ),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(15),
+              ),
+            ),
+            overlayColor: MaterialStatePropertyAll<Color>(
+              AppColors.primary.withOpacity(0.2),
+            ),
+          ),
+        ),
       ),
       home: const SplashPage(),
     );
