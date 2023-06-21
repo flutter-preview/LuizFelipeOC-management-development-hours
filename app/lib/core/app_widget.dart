5e103271429bb95f646260533713c45c6299202e
LuiFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-06-21 11:38:16 -0300

    feat: add new main widget

---
+++ app/lib/core/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../modules/splash/views/splash_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
