8fe1d783394c7d58b0f73151b0348c458ef616d6
LuiFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-06-22 11:25:14 -0300

    fix: :bug: fix broken welcome screen in keyboard up

--- app/lib/modules/welcome/views/welcome_page.dart
+++ app/lib/modules/welcome/views/welcome_page.dart
@@ -28,36 +28,81 @@ class _WelcomePageState extends State<WelcomePage> {
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(top: 50),
-                alignment: Alignment.topLeft,
-                child: ValueListenableBuilder(
-                  valueListenable: controller.animation1,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 50),
+                  alignment: Alignment.topLeft,
+                  child: ValueListenableBuilder(
+                    valueListenable: controller.animation1,
+                    builder: (BuildContext context, value, _) {
+                      return AnimatedCrossFade(
+                        secondCurve: Curves.easeInOut,
+                        duration: const Duration(milliseconds: 500),
+                        crossFadeState: value
+                            ? CrossFadeState.showSecond
+                            : CrossFadeState.showFirst,
+                        firstChild: Container(),
+                        secondChild: SvgWidget(
+                          pathSvg: AppImages.firstImage,
+                          width: screen.width * .50,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 50),
+                  alignment: Alignment.topRight,
+                  child: ValueListenableBuilder(
+                    valueListenable: controller.animation2,
+                    builder: (BuildContext context, value, _) {
+                      return AnimatedCrossFade(
+                        duration: const Duration(milliseconds: 500),
+                        firstChild: Container(),
+                        crossFadeState: value
+                            ? CrossFadeState.showSecond
+                            : CrossFadeState.showFirst,
+                        secondChild: SvgWidget(
+                          pathSvg: AppImages.secondaryImage,
+                          width: screen.width * .50,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 100),
+                ValueListenableBuilder(
+                  valueListenable: controller.animation3,
                   builder: (BuildContext context, value, _) {
                     return AnimatedCrossFade(
-                      secondCurve: Curves.easeInOut,
-                      duration: const Duration(milliseconds: 500),
                       crossFadeState: value
                           ? CrossFadeState.showSecond
                           : CrossFadeState.showFirst,
+                      duration: const Duration(milliseconds: 500),
                       firstChild: Container(),
-                      secondChild: SvgWidget(
-                        pathSvg: AppImages.firstImage,
-                        width: screen.width * .50,
+                      secondChild: Text.rich(
+                        textAlign: TextAlign.center,
+                        TextSpan(
+                          text: "WELCOME!\n ",
+                          style: AppTextStyles.heading500,
+                          children: [
+                            TextSpan(
+                              text: "WE HOPE THAT BE READY TO BEGIN.",
+                              style: AppTextStyles.heading,
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 50),
-                alignment: Alignment.topRight,
-                child: ValueListenableBuilder(
-                  valueListenable: controller.animation2,
+                const SizedBox(height: 45),
+                ValueListenableBuilder(
+                  valueListenable: controller.animation3,
                   builder: (BuildContext context, value, _) {
                     return AnimatedCrossFade(
                       duration: const Duration(milliseconds: 500),
@@ -65,79 +110,36 @@ class _WelcomePageState extends State<WelcomePage> {
                       crossFadeState: value
                           ? CrossFadeState.showSecond
                           : CrossFadeState.showFirst,
-                      secondChild: SvgWidget(
-                        pathSvg: AppImages.secondaryImage,
-                        width: screen.width * .50,
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 100),
-              ValueListenableBuilder(
-                valueListenable: controller.animation3,
-                builder: (BuildContext context, dynamic value, Widget? child) {
-                  return AnimatedCrossFade(
-                    crossFadeState: value
-                        ? CrossFadeState.showSecond
-                        : CrossFadeState.showFirst,
-                    duration: const Duration(milliseconds: 500),
-                    firstChild: Container(),
-                    secondChild: Text.rich(
-                      textAlign: TextAlign.center,
-                      TextSpan(
-                        text: "WELCOME!\n ",
-                        style: AppTextStyles.heading500,
+                      secondChild: Column(
                         children: [
-                          TextSpan(
-                            text: "WE HOPE THAT BE READY TO BEGIN.",
-                            style: AppTextStyles.heading,
-                          )
+                          SizedBox(
+                            height: 56,
+                            width: MediaQuery.of(context).size.width,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                showModalBottomSheet(
+                                  shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(15),
+                                      topRight: Radius.circular(15),
+                                    ),
+                                  ),
+                                  context: context,
+                                  builder: (_) => DialogAnimated(
+                                    controller: controller,
+                                  ),
+                                );
+                              },
+                              child: const Text("Start Now"),
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 45),
-              ValueListenableBuilder(
-                valueListenable: controller.animation3,
-                builder: (BuildContext context, value, _) {
-                  return AnimatedCrossFade(
-                    duration: const Duration(milliseconds: 500),
-                    firstChild: Container(),
-                    crossFadeState: value
-                        ? CrossFadeState.showSecond
-                        : CrossFadeState.showFirst,
-                    secondChild: Column(
-                      children: [
-                        SizedBox(
-                          height: 56,
-                          width: MediaQuery.of(context).size.width,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              showModalBottomSheet(
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(15),
-                                    topRight: Radius.circular(15),
-                                  ),
-                                ),
-                                context: context,
-                                builder: (_) => DialogAnimated(
-                                  controller: controller,
-                                ),
-                              );
-                            },
-                            child: const Text("Start Now"),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ],
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
