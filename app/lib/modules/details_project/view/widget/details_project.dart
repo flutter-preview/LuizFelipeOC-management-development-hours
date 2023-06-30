c417e2f2df26e8c10a87a949dc5fdb85d1276fe1
LuiFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-06-30 11:59:19 -0400

    refactor: :recycle: add onRrefreshIndicator e floatButtonAction

--- app/lib/modules/details_project/view/widget/details_project.dart
+++ app/lib/modules/details_project/view/widget/details_project.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../../core/shared/app_colors.dart';
 import '../../../../core/shared/app_text_styles.dart';
 import '../../../home/model/project_model.dart';
 import '../../controller/participantes_peoples_controller.dart';
 import 'list_participants_people_widgets.dart';
+import 'list_tasks_widget.dart';
 
 class DetailsProjects extends StatefulWidget {
   const DetailsProjects({super.key, required this.projects});
@@ -33,42 +35,74 @@ class _DetailsProjectsState extends State<DetailsProjects> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 70,
-            left: 20,
-            right: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
+        child: RefreshIndicator(
+          backgroundColor: AppColors.white,
+          color: AppColors.primary,
+          strokeWidth: 0.6,
+          edgeOffset: 50.0,
+          onRefresh: () async {},
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 70,
+                left: 20,
+                right: 20,
+                bottom: 30,
+              ),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  InkWell(
-                    onTap: () => Navigator.pop(context),
-                    child: const FaIcon(
-                      FontAwesomeIcons.arrowLeft,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: const FaIcon(
+                          FontAwesomeIcons.arrowLeft,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        widget.projects.title,
+                        style: AppTextStyles.heading,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        widget.projects.description,
+                        style: AppTextStyles.body500,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Created in: 26/06/2023",
+                        style: AppTextStyles.text500,
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 20),
-                  Text(
-                    widget.projects.title,
-                    style: AppTextStyles.heading,
+                  ListParticipantsPeoplesWidget(
+                    participantsController: participantsController,
                   ),
-                  const SizedBox(height: 5),
-                  Text(
-                    widget.projects.description,
-                    style: AppTextStyles.body500,
-                  ),
+                  const SizedBox(height: 20),
+                  const ListTasksWidget()
                 ],
               ),
-              const SizedBox(height: 40),
-              ListParticipantsPeoplesWidget(
-                participantsController: participantsController,
-              ),
-            ],
+            ),
           ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => {},
+        label: const Row(
+          children: [
+            FaIcon(
+              FontAwesomeIcons.plus,
+            ),
+            SizedBox(width: 20),
+            Text(
+              "TASK",
+            ),
+          ],
         ),
       ),
     );
