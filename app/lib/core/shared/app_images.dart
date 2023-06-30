7b3e03d676717006056bfb836d368b4f527a5942
LuiFelipeOC <luizfelipeeoliveiraac@gmail.com>
2023-06-30 11:20:33 -0400

    chore: :lipstick: add images to mock participants peoples

--- app/lib/core/shared/app_images.dart
+++ app/lib/core/shared/app_images.dart
@@ -1,4 +1,16 @@
 final class AppImages {
   static String get firstImage => 'assets/images/primary.svg';
   static String get secondaryImage => 'assets/images/secondary.svg';
+
+  //mock list others person
+  static String get person1 => 'assets/images/person1.jpeg';
+  static String get person2 => 'assets/images/person2.jpeg';
+  static String get person3 => 'assets/images/person3.jpeg';
+  static String get person4 => 'assets/images/person4.jpeg';
+  static String get person5 => 'assets/images/person5.png';
+  static String get person6 => 'assets/images/person6.jpg';
+  static String get person7 => 'assets/images/person7.jpeg';
+  static String get person8 => 'assets/images/person8.jpg';
+  static String get person9 => 'assets/images/person9.jpeg';
+  static String get person10 => 'assets/images/person10.jpeg';
 }
